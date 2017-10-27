e8612ff60543f5633a980d756d5159aa82bba1c2
Nikolay Sukhikh <nikolay.sukhih@sintez.co.za>
2017-10-27 12:53:23 +0300

    Remove video play button, use direct embed instead
    iOS Safari doesn't support video autoplay and custom play button
    requires 2 clicks to play the video. So it is simplier to use direct
    YouTube embed instead.

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,4 @@
 /**
- * YouTube API load callback
- */
-function onYouTubeIframeAPIReady() {
-	//play YouTube video on button click
-	$('#play-video').click(function() {
-		new YT.Player('player', {
-			width: '960',
-			height: '540',
-			videoId: 'ruTB5jBGXsE',
-			events: {
-				'onReady': onPlayerReady
-			}
-		});
-		
-		function onPlayerReady(event) {
-			event.target.playVideo();
-		}
-		
-		$(this).hide();
-		
-		return false;
-	});
-}
-
-/**
  * ICO counter
  */
 $(function() {
