a91019830af9cfa9743d95a912a2e074ba523c43
Nikolay Sukhikh <nikolay.sukhih@sintez.co.za>
2017-11-03 13:10:47 +0200

    Optimize scripts for the Whitepaper page

--- scripts/main.js
+++ scripts/main.js
@@ -6,25 +6,27 @@ $(function() {
 	var countDownDate = new Date("Dec 1, 2017 00:00:00").getTime();
 	var $timer = $('#timer');
 	
-	// Update the count down every 1 second
-	setInterval(function() {
-		// Get today's date and time
-		var now = new Date().getTime();
-		
-		// Find the distance between now an the count down date
-		var distance = countDownDate - now;
-		
-		// Time calculations for days, hours, minutes and seconds
-		var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-		
-		$timer.find('.timer-day').html(n(days));
-		$timer.find('.timer-hour').html(n(hours));
-		$timer.find('.timer-min').html(n(minutes));
-		$timer.find('.timer-sec').html(n(seconds));
-	}, 1000);
+	if ($timer.length) {
+		// Update the count down every 1 second
+		setInterval(function () {
+			// Get today's date and time
+			var now = new Date().getTime();
+			
+			// Find the distance between now an the count down date
+			var distance = countDownDate - now;
+			
+			// Time calculations for days, hours, minutes and seconds
+			var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+			var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+			var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+			var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+			
+			$timer.find('.timer-day').html(n(days));
+			$timer.find('.timer-hour').html(n(hours));
+			$timer.find('.timer-min').html(n(minutes));
+			$timer.find('.timer-sec').html(n(seconds));
+		}, 1000);
+	}
 	
 	function n(n){
 		return n > 9 ? "" + n: "0" + n;
@@ -38,7 +40,7 @@ $(function() {
 	var $header = $('#main-header');
 	
 	//change header style on scroll
-	$('#top-section').waypoint(function(dir) {
+	$('body > section:first-of-type').waypoint(function(dir) {
 		if (dir === 'down') {
 			$header.addClass('fixed-header');
 		} else {
@@ -233,6 +235,9 @@ $(function() {
 	
 	var $tokens = $('#tokens-chart'), tokensWidth = $tokens.width(),
 		$funds = $('#funds-chart'), fundsWidth = $funds.width();
+	
+	if (!$tokens.length && !$funds.length)
+		return;
 	
 	google.charts.load('current', {packages: ['corechart']});
 	google.charts.setOnLoadCallback(drawTokensChart);
