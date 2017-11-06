3325d3878fd62aca64730641e0367cd0d64a4a49
Nikolay Sukhikh <nikolay.sukhih@sintez.co.za>
2017-11-06 12:49:45 +0200

    Show subscribe form only once in a month

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,26 @@
+//https://www.w3schools.com/js/js_cookies.asp
+function setCookie(cname, cvalue, exdays) {
+	var d = new Date();
+	d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	var expires = "expires="+ d.toUTCString();
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+	var name = cname + "=";
+	var decodedCookie = decodeURIComponent(document.cookie);
+	var ca = decodedCookie.split(';');
+	for(var i = 0; i <ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
+
 /**
  * ICO counter
  */
@@ -106,6 +129,7 @@ $(function() {
 		lockBody();
 		$subFormShim.show();
 		$('#subscribe-email').focus();
+		setCookie('subscribe_form', '1', 30);
 	}
 	function hideSubForm() {
 		unlockBody();
@@ -130,7 +154,8 @@ $(function() {
 	});
 	if ($subForm.length) {
 		$('#timeline-section').waypoint(function() {
-			showSubForm();
+			if (!getCookie('subscribe_form'))
+				showSubForm();
 		});
 	}
 });
